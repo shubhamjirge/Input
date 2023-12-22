0234cf13903a5466bdcdf7755aed576292ae56dd
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2023-12-22 10:14:35 +0530

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,11 @@
+"""
+Title     : Input()
+Subdomain : Built-Ins
+Domain    : Python
+Problem   : https://www.hackerrank.com/challenges/input/problem
+"""
+
+if __name__ == "__main__":
+    x, k = map(int, input().strip().split())
+    equation = input().strip()
+    print(eval(equation) == k)
